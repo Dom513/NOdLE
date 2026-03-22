3621507e49b89aff49ffb0afd94d14b8aec0e326
Dom513 <dominicwilde3@gmail.com>
2026-03-22 10:49:59 +0000

    add app and tracking

---
+++ nodle-android/app/src/main/java/com/dwgame/nodle/DelegationService.java
@@ -0,0 +1,14 @@
+package com.dwgame.nodle;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
